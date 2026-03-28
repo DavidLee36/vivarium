68f88b79509d737eed9e2126256c18992463292b
DavidLee36 <david303lee@gmail.com>
2026-03-27 23:00:40 -0700

    initial commit

---
+++ scripts/camera.gd
@@ -0,0 +1,42 @@
+extends Camera3D
+
+@export var default_speed: float = 10
+@export var sensitivity: float = 0.3
+
+var _mouse_delta := Vector2.ZERO
+@onready var _pitch: float = rad_to_deg(rotation.x)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_mouse_delta = event.relative
+
+func _physics_process(delta: float) -> void:
+	_movement_logic(delta)
+	_mouse_delta = Vector2.ZERO
+
+func _movement_logic(delta: float) -> void:
+	# Speed
+	var speed = default_speed * 2 if Input.is_action_pressed("shift") else default_speed
+	
+	# WASD
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var move := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	position += move * speed * delta
+	if position.y <= 0: position.y = 0.1
+	
+	# Vertical
+	if Input.is_action_pressed("space_bar"):
+		position.y += speed * delta
+	elif Input.is_action_pressed("ctrl"):
+		position.y -= speed * delta
+	
+	# PANNING
+	if Input.is_action_pressed("right_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		rotate_y(deg_to_rad(-_mouse_delta.x * sensitivity))
+		_pitch = clamp(_pitch - _mouse_delta.y * sensitivity, -90, 90)
+		rotation.x = deg_to_rad(_pitch)
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	if position.y <= 0.1: position.y = 0.1
---
+++ scripts/mote.gd
@@ -0,0 +1,62 @@
+extends CharacterBody3D
+
+enum State {
+	WANDERING,
+	SEEKING_FOOD,
+	SEEKING_WATER
+}
+
+var state: State = State.WANDERING
+@onready var target_position: Vector3 = global_position # Currently we only use x, z but eventually we may use y axis?
+var rotation_speed_min: float = 1.5
+var rotation_speed_max: float = 5.0
+
+var move_r: int = 20
+
+var sight_r: int = 10
+var speed: int = 3
+
+var food: float = 100
+var water: float = 100
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	_update_curr_state()
+	_movement_logic(delta)
+
+func _movement_logic(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= 20 * delta # TODO: Remove magic number
+	if state == State.WANDERING:
+		_wander()
+
+	var dir = (target_position - global_position).normalized()
+	var flat_dir = Vector3(dir.x, 0, dir.z).normalized()
+	if flat_dir.length() > 0.1:
+		var turn_angle = rad_to_deg((-transform.basis.z).angle_to(flat_dir))
+		var target_basis := Basis.looking_at(flat_dir)
+		transform.basis = transform.basis.slerp(target_basis, delta * remap(turn_angle, 0, 180, rotation_speed_min, rotation_speed_max))
+	velocity = lerp(velocity, dir * speed, delta * 2.0)
+
+	move_and_slide()
+
+func _update_curr_state() -> void:
+	pass
+
+func _wander() -> bool:
+	if _at_position_buffer(0.05) or (global_position.x == 0 and global_position.z == 0): # Pick a new target pos
+		target_position.x = randi_range(int(global_position.x) - move_r, int(global_position.x) + move_r)
+		target_position.z = randi_range(int(global_position.z) - move_r, int(global_position.z) + move_r)
+		return true
+	return false
+
+func _seek_resource() -> void:
+	pass
+
+func _at_position_buffer(buffer: float) -> bool:
+	# Currently only deal with x and z
+	if abs(global_position.x - target_position.x) < buffer and abs(global_position.z - target_position.z) < buffer:
+		return true
+	return false
